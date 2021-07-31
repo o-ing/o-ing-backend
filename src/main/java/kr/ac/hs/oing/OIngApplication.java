8d2adcda47947ff88113f6289eea907b35b84b9e
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-08-01 00:38:28 +0900

    [SETUP] PROJECT INIT

---
+++ src/main/java/kr/ac/hs/oing/OIngApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hs.oing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OIngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OIngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/hs/oing/OIngApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hs.oing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OIngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
