0957a5ebde7b37a66ab7f79dc2831ff8d9185bd4
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-08-28 14:18:42 +0900

    jwt 적용

--- src/main/java/kr/ac/hs/oing/config/SecurityConfig.java
+++ src/main/java/kr/ac/hs/oing/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package kr.ac.hs.oing.config;
 
 import kr.ac.hs.oing.auth.AuthPrincipalService;
+import kr.ac.hs.oing.auth.JwtAuthenticationFilter;
+import kr.ac.hs.oing.auth.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,7 +25,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthPrincipalService authPrincipalService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     @Override
@@ -35,38 +38,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder encodePWD() {
         return new BCryptPasswordEncoder();
     }
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable();
-
-        http
-                .authorizeRequests()
-                .antMatchers("/index", "/", "/auth/**", "/js/**", "/css/**", "/image/**") // ::TODO 동진이껄로 변경해야함
-                .permitAll()
-                .anyRequest()
-                .authenticated()
+                .httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제하겠습니다.
+                .csrf().disable() // csrf 보안 토큰 disable처리.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용하지 않습니다.
                 .and()
-                .exceptionHandling();
-
-        http
-                .formLogin()
-                .loginPage("/auth/login") // ::TODO 동진이껄로 변경해야함
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .loginProcessingUrl("/auth/loginProc") // ::TODO 동진이껄로 변경해야함
-                .defaultSuccessUrl("/") // ::TODO 동진이껄로 변경해야함
+                .authorizeRequests() // 요청에 대한 사용권한 체크
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .anyRequest().permitAll() // 그외 나머지 요청은 누구나 접근 가능
                 .and()
-                .exceptionHandling();
-
-        http
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // ::TODO 동진이껄로 변경해야함
-                .logoutSuccessUrl("/index")
-                .invalidateHttpSession(true)
-                .and()
-                .exceptionHandling();
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
 
     }
 }
