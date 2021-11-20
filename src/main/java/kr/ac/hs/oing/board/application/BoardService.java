2cb44a8709b58850bf2c797defc312a523618c94
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-11-20 22:25:02 +0900

    기초 구성 완료

--- src/main/java/kr/ac/hs/oing/board/application/BoardService.java
+++ src/main/java/kr/ac/hs/oing/board/application/BoardService.java
@@ -21,9 +21,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
